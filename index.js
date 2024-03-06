14d4caa3e5827f386916ce60d5fe27596bbdc42e
Migue Diaz <skills20003@gmail.com>
2024-03-05 22:42:26 -0500

    secuencia numerica

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let arrayVacio = []//array vacio
+
 let arrayFrutas = [manzana, pera, mango, sandia]//array con elementos 
+
 let arrayElmDif = ['Miguel', 5, false]//array con diferentes tipos de datos
+
 var miArray = [];//Crear un array utilizando el constructor array
 var otroArray = [1, 2, 3, 4, 5];
 var arrayConLongitud = new Array(10); 
 var arrayVacio2 = new Array(5);
+
+// Crear un array con una secuencia numérica usando Array.from()
+var secuenciaNumerica = Array.from({ length: 10 }, (_, index) => index)
+console.log(secuenciaNumerica)
+
+
+
