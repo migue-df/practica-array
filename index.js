0787754c3d7d8ceeaadfc890c3e8466a56f7d9a0
Migue Diaz <skills20003@gmail.com>
2024-03-05 22:36:10 -0500

    constructor array

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let arrayVacio = []//array vacio
 let arrayFrutas = [manzana, pera, mango, sandia]//array con elementos 
 let arrayElmDif = ['Miguel', 5, false]//array con diferentes tipos de datos
+var miArray = [];//Crear un array utilizando el constructor array
+var otroArray = [1, 2, 3, 4, 5];
+var arrayConLongitud = new Array(10); 
+var arrayVacio2 = new Array(5);
